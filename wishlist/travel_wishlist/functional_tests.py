3bb6c28f42c5a5d7ac3f8970b00c3f9794c0fa13
a1d3n <$>
2022-11-14 00:38:20 -0600

    Got selenium working. All functional tests pass except for part of one that gives an attribute err (webdriver object has no attribute find_element_by_id, it says)

--- wishlist/travel_wishlist/functional_tests.py
+++ wishlist/travel_wishlist/functional_tests.py
@@ -1,15 +1,12 @@
-# from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.firefox.webdriver import WebDriver
 from django.test import LiveServerTestCase
-from selenium import webdriver
-
-# browser = webdriver.Chrome()
-browser = webdriver.Chrome(executable_path=r"C:\Users\aiden\chromedriver_win32")
 
 
 class TitleTest(LiveServerTestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
+        cls.selenium = WebDriver()
         cls.selenium.implicitly_wait(10)
 
     @classmethod
@@ -20,3 +17,34 @@ class TitleTest(LiveServerTestCase):
     def test_title_on_home_page(self):
         self.selenium.get(self.live_server_url)
         self.assertIn("Travel Wishlist", self.selenium.title)
+
+
+class AddPlacesTest(LiveServerTestCase):
+
+    fixtures = ["test_places"]
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.selenium = WebDriver()
+        cls.selenium.implicitly_wait(10)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super().tearDownClass()
+
+    def test_add_new_place(self):
+        self.selenium.get(self.live_server_url)
+        input_name = self.selenium.find_element_by_id("id_name")
+        input_name.send_keys("Denver")
+
+        add_button = self.selenium.find_element_by_id("add-new-place")
+        add_button.click()
+
+        denver = self.selenium.find_element_by_id("place-name-5")
+        self.assertEqual("Denver", denver.text)
+
+        self.assertIn("Denver", self.selenium.page_source)
+        self.assertIn("New York", self.selenium.page_source)
+        self.assertIn("Tokyo", self.selenium.page_source)
